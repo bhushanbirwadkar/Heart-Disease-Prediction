06ef7d15e69699367eade70a67c97fae4f5708a6
Bhushan Birwadkar <bhushanbirwadkar01@gmail.com>
2021-08-05 23:46:25 +0530

    files updated

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"bhushanbirwadkar01@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
